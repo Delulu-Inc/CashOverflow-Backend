f763a977cdbeb5c3cc46ce53485f45c99c03b814
OmarEL-Deeb <omar_eldeeb@gmail.com>
2026-09-10 17:50:49 +0300

    Adding Payment relations

--- CashOverflow/src/CashOverflow.Api/Program.cs
+++ CashOverflow/src/CashOverflow.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using CashOverflow.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 namespace CashOverflow.Api
 {
     public class Program
@@ -7,23 +10,24 @@ namespace CashOverflow.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
+
             builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
 
             app.MapControllers();
 
