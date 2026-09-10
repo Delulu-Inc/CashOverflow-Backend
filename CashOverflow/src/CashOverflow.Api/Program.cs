a56d45c427cb3edc53656a26dc623a515f6dea67
shahdgaballah <shahdgabllah@gmail.com>
2026-09-11 01:05:59 +0300

    implement auth endpoints

--- CashOverflow/src/CashOverflow.Api/Program.cs
+++ CashOverflow/src/CashOverflow.Api/Program.cs
@@ -1,7 +1,12 @@
+using CashOverflow.Application.Common.Interfaces;
 using CashOverflow.Domain.Entities;
+using CashOverflow.Infrastructure.Identity;
 using CashOverflow.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +15,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<IJwtService, JwtService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 //Add ApplicationDbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -22,6 +29,47 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+//Add jwt
+
+builder.Services.AddAuthentication(
+     options =>
+     {
+         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+     }
+
+    ).AddJwtBearer(
+     options =>
+     {
+         options.TokenValidationParameters = new TokenValidationParameters
+         {
+             ValidateIssuer = true,
+             ValidateAudience = true,
+             ValidateLifetime = true,
+             ValidateIssuerSigningKey = true,
+
+             ValidIssuer = builder.Configuration["Jwt:Issuer"],
+             ValidAudience = builder.Configuration["Jwt:Audience"],
+
+             IssuerSigningKey = new SymmetricSecurityKey(
+                 Encoding.UTF8.GetBytes(
+                     builder.Configuration["Jwt:SecretKey"]!
+                 )
+             )
+         };
+
+         //print the error related to jwt configuring in the console
+         options.Events = new JwtBearerEvents
+         {
+             OnAuthenticationFailed = context =>
+             {
+                 Console.WriteLine("JWT ERROR: " + context.Exception.Message);
+                 return Task.CompletedTask;
+             }
+         };
+     }
+
+    );
 
 var app = builder.Build();
 
@@ -33,8 +81,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseAuthentication();
 
+app.UseAuthorization();
 
 app.MapControllers();
 
