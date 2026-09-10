86fc06dac36e41ba745887b9dee5c4037b0af08e
OmarEL-Deeb <omar_eldeeb@gmail.com>
2026-09-10 11:16:22 +0300

    Configure SQL Server DbContext and API dependency injection

--- CashOverflow/src/CashOverflow.Api/Program.cs
+++ CashOverflow/src/CashOverflow.Api/Program.cs
@@ -10,7 +10,6 @@ namespace CashOverflow.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
